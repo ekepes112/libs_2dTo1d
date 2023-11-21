aedea8bfa9228c13538cf32a2e063156147ca80a
ekepes112 <kepes.erik@gmail.com>
2023-11-21 20:01:08 +0100

    fix: config file

--- src/config.py
+++ src/config.py
@@ -3,3 +3,16 @@ PROJECT_NAME = "transferLearning_2dTo1d"
 BATCH_SIZE = 64
 EPOCH_COUNT = 500
 MSE_LOW_THRESHOLD = 15
+
+PROJECT_NAME = "libs_2dTo1d"
+DRIVE_PATH = '/content/onedrive'
+RESULTS_PATH = '/content/gdrive/My Drive/projects/libs_2dTo1d/temp'
+DATA_PATH = '.archive/datasets/'
+CHECKPOINT_DIR = '/content/checkpoints'
+
+DATASET_TYPE = 'means'
+TRAIN_FOLDS = [1, 2, 4, 5]
+TEST_FOLD = 3
+COMPOUND_LIST = ['SiO2', 'TiO2', 'Al2O3', 'FeOT', 'MgO', 'CaO', 'Na2O', 'K2O']
+SHIFT_MAGNITUDES = [-3, -2, -1, 1, 2, 3]
+NORMALIZE_TO_UNIT_MAXIMUM = False
